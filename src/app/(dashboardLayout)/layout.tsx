054e7f94fb82841a46717f1b1b2bffd3cba8ec93
aticcmahbub <aticcmahbub@gmail.com>
2026-01-09 00:09:34 +0600

    feat: configure dashboard layout

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,17 +1,21 @@
-import LogoutButton from '@/components/shared/LogoutButton';
-import {getCookie} from '@/services/auth/tokenHandlers';
 import React from 'react';
 
-export default async function DashboardLayout({
+const CommonDashboardLayout = async ({
     children,
 }: {
     children: React.ReactNode;
-}) {
-    const accessToken = await getCookie('accessToken');
+}) => {
     return (
-        <div>
-            {accessToken && <LogoutButton />}
-            {children}
+        <div className='flex h-screen overflow-hidden'>
+            {/* <DashboardSidebar /> */}DashboardSidebar
+            <div className='flex flex-1 flex-col overflow-hidden'>
+                {/* <DashboardNavbar /> */}DashboardNavbar
+                <main className='flex-1 overflow-y-auto bg-muted/10 p-4 md:p-6'>
+                    <div className='max-w-7xl'>{children}</div>
+                </main>
+            </div>
         </div>
     );
-}
+};
+
+export default CommonDashboardLayout;
